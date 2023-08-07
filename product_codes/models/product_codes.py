4b02b077e6680ac3a74c46e459fe8905162c565f
Gianfranco De Paula <baciliogianfranco@gmail.com>
2023-08-07 11:17:45 -0400

    [ADD] fields translation

--- product_codes/models/product_codes.py
+++ product_codes/models/product_codes.py
@@ -3,6 +3,6 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    old_code = fields.Char(string='Old Code')
-    provider_code = fields.Char(string='Provider Code')
-    custom_code = fields.Char(string='Custom Code')
+    old_code = fields.Char(string='Old code')
+    provider_code = fields.Char(string='Provider code')
+    custom_code = fields.Char(string='Custom code')
